48ec79a1ef1e4cc32cabea19a36e1a57b00ae724
ding <playdingnow@gmail.com>
2018-03-05 15:56:15 +0800

    feat(entrypoint): read cron log level from environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
+loglevel=${cronloglevel:-0}
 cronjob=${cronjob:-'*/1 * * * * echo $(date) > /dev/stdout'}
 
 echo "your job is $cronjob"
 echo "$cronjob" > /tmp/cronjob
 /usr/bin/crontab /tmp/cronjob
 
-/usr/sbin/crond -f -l 0
+/usr/sbin/crond -f -l $loglevel
