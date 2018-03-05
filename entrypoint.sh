edef6a47048d4d98c4334eea87d90f8657d9feab
ding <playdingnow@gmail.com>
2018-03-05 15:46:22 +0800

    fix: missing "" acount crontjob

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-cronjob=${cronjob:-'*/1 * * * * echo "test" >> /dev/stdout'}
+cronjob=${cronjob:-'*/1 * * * * echo $(date) > /dev/stdout'}
 
 echo "your job is $cronjob"
-echo $cronjob > /tmp/cronjob
+echo "$cronjob" > /tmp/cronjob
 /usr/bin/crontab /tmp/cronjob
 
 /usr/sbin/crond -f -l 0
